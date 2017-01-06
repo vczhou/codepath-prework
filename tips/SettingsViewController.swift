6aa2f8924e9078f04018fa16d60be1426c91daf7
vczhou <victoria.ch.zhou@gmail.com>
2017-01-06 12:05:27 -0600

    basic tip calculator

---
+++ tips/SettingsViewController.swift
@@ -0,0 +1,105 @@
+//
+//  SettingsViewController.swift
+//  tips
+//
+//  Created by Victoria Zhou on 3/11/16.
+//  Copyright (c) 2016 Victoria Zhou. All rights reserved.
+//
+
+import UIKit
+let tipsValues = NSUserDefaults.standardUserDefaults()
+
+class SettingsViewController: UIViewController {
+    
+    @IBOutlet weak var highField: UITextField!
+    @IBOutlet weak var midField: UITextField!
+    @IBOutlet weak var lowField: UITextField!
+    @IBOutlet weak var saveButton: UIButton!
+    var set = false;
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        //println("view will appear")
+        if(!set){
+            lowField.text = "18"
+            midField.text = "20"
+            highField.text = "22"
+            set = true
+        }else{
+            lowField.text = tipsValues.objectForKey("one") as! String
+            midField.text = tipsValues.objectForKey("two") as! String
+            highField.text = tipsValues.objectForKey("three") as! String
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func onSettingsTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func updateLow(sender: AnyObject) {
+        if(lowField.text.isEmpty){
+            lowField.text = "18"
+        }
+        tipsValues.setObject(lowField.text, forKey: "one")
+        tipsValues.synchronize()
+    }
+
+    @IBAction func updateMid(sender: AnyObject) {
+        if(midField.text.isEmpty){
+            midField.text = "20"
+        }
+        tipsValues.setObject(midField.text, forKey: "two")
+        tipsValues.synchronize()
+    }
+    
+    @IBAction func updateHigh(sender: AnyObject) {
+        if(highField.text.isEmpty){
+            highField.text = "22"
+        }
+        tipsValues.setObject(highField.text, forKey: "three")
+        tipsValues.synchronize()
+    }
+    
+    @IBAction func updateAll(sender: AnyObject) {
+        
+        if(lowField.text.isEmpty){
+            lowField.text = "18"
+        }
+        tipsValues.setObject(lowField.text, forKey: "one")
+        tipsValues.synchronize()
+        
+        if(midField.text.isEmpty){
+            midField.text = "20"
+        }
+        tipsValues.setObject(midField.text, forKey: "two")
+        tipsValues.synchronize()
+        
+        if(highField.text.isEmpty){
+            highField.text = "22"
+        }
+        tipsValues.setObject(highField.text, forKey: "three")
+        tipsValues.synchronize()
+        
+    }
+}
--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -10,16 +10,90 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipController: UISegmentedControl!
+    
+    var set = false
+    var tipPercentages = [0.18, 0.20, 0.22]
+    var lowString = ""
+    var midString = ""
+    var highString = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func setTipStrings(){
+        lowString = tipsValues.objectForKey("one") as! String
+        midString = tipsValues.objectForKey("two") as! String
+        highString = tipsValues.objectForKey("three") as! String
+    }
+    
+    func editSegmentView(){
+        tipController.setTitle(lowString + "%", forSegmentAtIndex: 0)
+        tipController.setTitle(midString + "%", forSegmentAtIndex: 1)
+        tipController.setTitle(highString + "%", forSegmentAtIndex: 2)
+    }
+    
+    func setTipVals(){
+        tipPercentages[0] =  (NSString(string: lowString).doubleValue) / 100
+        tipPercentages[1] =  (NSString(string: midString).doubleValue) / 100
+        tipPercentages[2] =  (NSString(string: highString).doubleValue) / 100
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        //print("view will appear")
+        if(set){
+            setTipStrings()
+            editSegmentView()
+            setTipVals()
+        }
+        else{
+            set = true;
+        }
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        billField.becomeFirstResponder()
+        //print("view did appear")
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        //print("view will disappear")
+    }
+    
+    override func viewDidDisappear(animated: Bool) {
+        super.viewDidDisappear(animated)
+        //print("view did disappear")
+    }
 
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        var tipPercentage = tipPercentages[tipController.selectedSegmentIndex]
+        
+        let billAmount = NSString(string: billField.text!).doubleValue
+        
+        let tip = billAmount * tipPercentage
+        let total = billAmount + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
 }
 
